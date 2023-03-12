ec063fe2e17f046949da516ca58d2be4e8aa822f
xkq957 <xu.kq@qq.com>
2023-03-12 23:34:21 +0800

    add configuration

--- gmall-order/src/main/java/com/xkq/gmall/order/GmallOrderApplication.java
+++ gmall-order/src/main/java/com/xkq/gmall/order/GmallOrderApplication.java
@@ -1,8 +1,10 @@
 package com.xkq.gmall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.xkq.gmall.order.dao")
 @SpringBootApplication
 public class GmallOrderApplication {
 
