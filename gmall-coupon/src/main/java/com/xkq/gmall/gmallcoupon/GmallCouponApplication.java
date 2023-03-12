290a8dd2066cfad97a8a8932cc084fdc78a9b1a5
xkq957 <xu.kq@qq.com>
2023-03-12 18:20:42 +0800

    init project

---
+++ gmall-coupon/src/main/java/com/xkq/gmall/gmallcoupon/GmallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.xkq.gmall.gmallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallCouponApplication.class, args);
+	}
+
+}
---
+++ gmall-ware/src/test/java/com/xkq/gamll/ware/GmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkq.gamll.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
