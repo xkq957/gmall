7058a75fb42b3dc97f7f14e5c3e1beaaf6ab3b23
xkq957 <xu.kq@qq.com>
2023-03-14 01:12:32 +0800

    add Category list tree

--- gmall-product/src/main/java/com/xkq/gmall/product/service/impl/CategoryServiceImpl.java
+++ gmall-product/src/main/java/com/xkq/gmall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.xkq.gmall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +28,49 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 查询树形三级分类
+     * @return
+     */
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 查出所有分类
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+
+        //组装成父子的树形结构
+        //找到所有的一级分类
+        List<CategoryEntity> collect = categoryEntities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map((item) -> {
+                    item.setChildren(getChildren(item, categoryEntities));
+                    return item;
+                }).sorted((item1, item2) -> {
+                    return (item1.getSort() == null ? 0 : item1.getSort()) - (item2.getSort() == null ? 0 : item2.getSort());
+                }).collect(Collectors.toList());
+
+        return collect;
+    }
+
+    /**
+     * 递归查询所有菜单的子菜单
+     * @param root
+     * @param list
+     * @return
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> list) {
+        List<CategoryEntity> collect = list.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId())
+                .map(categoryEntity -> {
+                    //1.找到子菜单
+                    categoryEntity.setChildren(getChildren(categoryEntity, list));
+                    return categoryEntity;
+                }).sorted((item1, item2) -> {
+                    //2.菜单排序
+                    return (item1.getSort() == null ? 0 : item1.getSort()) - (item2.getSort() == null ? 0 : item2.getSort());
+                }).collect(Collectors.toList());
+        return collect;
     }
 
 }
